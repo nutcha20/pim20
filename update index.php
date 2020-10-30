5f1f0503165818f4ddae5f5ac9b7082b2747a36f
nutcha20 <73681375+nutcha20@users.noreply.github.com>
2020-10-30 12:48:29 +0700

    Create update index.php

---
+++ update index.php
@@ -0,0 +1,20 @@
+<hlme>
+    <html>
+        <title>Profile</title>
+    </head>
+    <body>
+        <h1>Profile</h1>
+        <p>ชื่อ : ณัชชา ธีระแนว ชื่อเล่น : พิม</p>
+        <img src="cat.jpg">
+        <br>
+        <a href ="https://www.it.kmitl.ac.th">it kmitl!!!</a>
+
+        <h2>The unordered list</h2>
+        <ul>
+            <li>Google</li>
+            <li>Yahoo</li>
+            <li>Amazon</li>
+        </ul>
+    
+    </body>
+</html>
