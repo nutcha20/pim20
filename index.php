edd734e907b002758e72ad4a982d98a0f3ee536f
nutcha20 <73681375+nutcha20@users.noreply.github.com>
2020-10-30 12:52:33 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,24 @@
-<?php
-echo "<h2>PHP is Fun</h2>";
-echo "Hello world!<br>";
-?>
+<hlme>
+    <html>
+        <title>Profile</title>
+    </head>
+    <body>
+        <h1>Profile</h1>
+        <p>ชื่อ : ณัชชา ธีระแนว ชื่อเล่น : พิม</p>
+        <img src="cat2.jpg">
+        <img src="cat3.jpg">
+        <br>
+        <a href ="https://www.it.kmitl.ac.th">it kmitl!!!</a>
+        <p>555555555555555555555555555555555555555555555555<p>
+        <p>555555555555555555555555555555555555555555555555<p>
+        <p>555555555555555555555555555555555555555555555ขำจังเรย<p>
+
+        <h2>The unordered list</h2>
+        <ul>
+            <li>Google</li>
+            <li>Yahoo</li>
+            <li>Amazon</li>
+        </ul>
+    
+    </body>
+</html>
