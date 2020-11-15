728dd84727e8b22c37408b5480d7a2d4f0952b74
nutcha20 <73681375+nutcha20@users.noreply.github.com>
2020-11-15 21:57:06 +0700

    Add files via upload

--- itshow.php
+++ itshow.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-<title>Write Your Comment</title>
+<title>How are you today</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
@@ -21,13 +21,13 @@ $res = mysqli_query($conn, 'SELECT * FROM NewTable_1');
 
 <div class="container">
     <div class="form">
-        <p class="ex1"><h1><center>I want you to somrthink</center></h1></p>
+        <p class="ex1"><h1><center>How are you today</center></h1></p>
     </div>
   <table width="800" border="1" class="table table-dark table-hover table-striped" class="center" >
     <thead class="thead-dark">
     <tr class="active">
       <th width="200"> <div align="center">Name</div></th>
-      <th width="500"> <div align="center">Comment</div></th>
+      <th width="500"> <div align="center">Felt</div></th>
       <th width="100"> <div align="center">Action</div></th>
     </tr>
      </thead>
@@ -44,7 +44,7 @@ $res = mysqli_query($conn, 'SELECT * FROM NewTable_1');
   }
   ?>
   </table>
-  <a href="forminsert.html"><input type="submit" value="ADD" class="btn-default btn-sm"></a>
+  <a href="plus.html"><input type="submit" value="ADD" class="btn-default btn-sm"></a>
   <?php
   mysqli_close($conn);
   ?>
