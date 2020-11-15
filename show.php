226259be4d675aa46bdfa9e3614df3037d88af3a
nutcha20 <73681375+nutcha20@users.noreply.github.com>
2020-11-15 12:55:41 +0700

    Update show.php

--- show.php
+++ show.php
@@ -10,13 +10,13 @@ if (mysqli_connect_errno($conn))
 {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
-$res = mysqli_query($conn, 'SELECT * FROM guestbook');
+$res = mysqli_query($conn, 'SELECT * FROM newtable');
 ?>
 <table width="600" border="1">
   <tr>
     <th width="100"> <div align="center">Name</div></th>
     <th width="350"> <div align="center">Comment </div></th>
-    <th width="150"> <div align="center">Link </div></th>
+    <th width="150"> <div align="center">link </div></th>
   </tr>
 <?php
 while($Result = mysqli_fetch_array($res))
@@ -25,7 +25,7 @@ while($Result = mysqli_fetch_array($res))
   <tr>
     <td><?php echo $Result['Name'];?></div></td>
     <td><?php echo $Result['Comment'];?></td>
-    <td><?php echo $Result['Link'];?></td>
+    <td><?php echo $Result['link'];?></td>
   </tr>
 <?php
 }
