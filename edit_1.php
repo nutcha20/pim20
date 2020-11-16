807887f901a81244ab6342345d2f6e811e91bc13
nutcha20 <73681375+nutcha20@users.noreply.github.com>
2020-11-17 00:53:55 +0700

    Update edit_1.php

--- edit_1.php
+++ edit_1.php
@@ -12,7 +12,7 @@ $name = $_POST['name'];
 $comment = $_POST['comment'];
 
 
-$sql = "UPDATE guestbook SET Comment='$comment' WHERE Name='$name' ";
+$sql = "UPDATE gust SET Comment='$comment' WHERE Name='$name' ";
 
 
 if (mysqli_query($conn, $sql)) {
