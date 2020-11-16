a6160b0ff9dea812058be983ef5d54489b9aa900
nutcha20 <73681375+nutcha20@users.noreply.github.com>
2020-11-17 00:34:21 +0700

    Update edit_1.php

--- edit_1.php
+++ edit_1.php
@@ -1,18 +1,18 @@
 <?php
 
 $conn = mysqli_init();
-mysqli_real_connect($conn, 'nutcha20.mysql.database.azure.com', 'nutcha@nutcha20', '20092544a-a', 'itflab', 3306);
+mysqli_real_connect($conn, 'nutcha20.mysql.database.azure.com', 'nutcha@nutcha20', '20092544a-a', 'ITFLab', 3306);
 if (mysqli_connect_errno($conn))
 {
     die('Failed to connect to MySQL PLEASE TRY AGAIN: '.mysqli_connect_error());
 }
 
 
-$name = $_POST['name'];
-$comment = $_POST['comment'];
+$name = $_POST['Name'];
+$comment = $_POST['Comment'];
 
 
-$sql = "UPDATE gust SET Comment='$comment' WHERE Name='$name' ";
+$sql = "UPDATE guestbook SET Comment='$comment' WHERE Name='$name' ";
 
 
 if (mysqli_query($conn, $sql)) {
@@ -22,4 +22,4 @@ if (mysqli_query($conn, $sql)) {
   }
   
 mysqli_close($conn);
-?
+?>
